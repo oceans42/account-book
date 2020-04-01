a8f239c43631e77514c9949c87b3ba4624b40d13
sparks42 <958575100@qq.com>
2020-04-01 23:22:00 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -47,14 +47,13 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      console.log(123);
+      recordListModel.create(this.record);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
